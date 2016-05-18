a846319f4d505160ecf3ed0f17a5f8cbddc69c49
Tom Dunn <tdunn@artofcontext.com>
2016-05-18 16:39:36 -0400

    Cache file resource objects in hash object.  If same resource request comes, just use already created file resource object.  Debug timing for performance changes.

--- angular-xhr-src.js
+++ angular-xhr-src.js
@@ -3,12 +3,21 @@
 
 	function xhrSrc($http, $window) {
 		var directive = {
-			restrict: 'A',
-			scope: true,
-			template: '',
-			replace: true,
-			link: linkFunction
-		};
+				restrict: 'A',
+				scope: true,
+				template: '',
+				replace: true,
+				link: linkFunction
+			},
+			fileCache = {},
+			count = 0;
+
+		window.showTime = function() {
+			console.log((window.lastTime - window.firstTime) / 1000);
+
+			count = 0;
+			window.firstTime = window.lastTime = undefined;
+		}
 
 		return directive;
 
@@ -19,19 +28,34 @@
 			attrs.$observe('xhrHref', sourceAttributeObserver);
 
 			function sourceAttributeObserver(source) {
+				var cachedFileObject;
+
 				if (source !== undefined) {
+					cachedFileObject = fileCache[source];
+
+					if (cachedFileObject !== undefined) {
+						elementAssignResource(elt, cachedFileObject);
+					}
+					if (count === 0) {
+						window.firstTime = window.lastTime = +(new Date());
+					}
+					if (cachedFileObject !== undefined) {
+						window.lastTime = +(new Date());
+						count++;
+
+						return;
+					}
+
 					$http.get(source, {responseType: 'blob'})
 						.then(
 							function(response) {
-								if (elt.tagName === 'LINK') {
-									elt.href = $window.URL.createObjectURL(response.data);
-								}
-								else if (elt.tagName === 'IMG') {
-									elt.src = $window.URL.createObjectURL(response.data);
-								}
-								else {
-									throw new Error('xhrSrc directive only supports setting LINK href and IMG src');
-								}
+								fileCache[source] = $window.URL.createObjectURL(response.data);
+								elementAssignResource(elt, fileCache[source]);
+
+								window.lastTime = +(new Date());
+
+								//console.log('xhrSrc: ' + count++);
+								count++;
 							},
 							function(result) {
 								var data = typeof(result.data) === "object" ? JSON.stringify(result.data) : result.data;
@@ -41,17 +65,37 @@
 						);
 				}
 			}
+
+			function elementAssignResource(element, resource) {
+				if (element.tagName === 'LINK') {
+					element.href = resource;
+				}
+				else if (element.tagName === 'IMG') {
+					element.src = resource;
+				}
+				else {
+					throw new Error('xhrSrc directive only supports setting LINK href and IMG src');
+				}
+			}
 		}
 	}
 
 	function xhrArrayBufferSrc($http, $window) {
 		var directive = {
-			restrict: 'A',
-			scope: true,
-			template: '',
-			replace: true,
-			link: linkFunction
-		};
+				restrict: 'A',
+				scope: true,
+				template: '',
+				replace: true,
+				link: linkFunction
+			},
+			count = 0;
+
+		window.showTime = function() {
+			console.log((window.lastTime - window.firstTime) / 1000);
+
+			count = 0;
+			window.firstTime = window.lastTime = undefined;
+		}
 
 		return directive;
 
@@ -63,12 +107,19 @@
 
 			function sourceAttributeObserver(source) {
 				if (source !== undefined) {
+
+					if (count === 0) {
+						window.firstTime = window.lastTime = +(new Date());
+					}
+
 					$http.get(source, {responseType: 'arraybuffer'})
 						.then(
 							function(response) {
 								var uInt8Array = new Uint8Array(response.data);
 								var i = uInt8Array.length;
 								var binaryString = new Array(i);
+								var imageType, lowercaseSource;
+
 								while (i--)
 								{
 									binaryString[i] = String.fromCharCode(uInt8Array[i]);
@@ -77,14 +128,31 @@
 								var base64 = window.btoa(data);
 
 								if (elt.tagName === 'LINK') {
-									elt.href = "data:text/css;base64," + base64;
+									elt.href = 'data:text/css;base64,' + base64;
 								}
 								else if (elt.tagName === 'IMG') {
-									elt.src = "data:image/png;base64," + base64;
+									lowercaseSource = source.toLowerCase();
+
+									if (lowercaseSource.indexOf('.jpg') !== -1) {
+										imageType = 'image/jpg';
+									}
+									else if (lowercaseSource.indexOf('.gif') !== -1) {
+										imageType = 'image/gif';
+									}
+									else {
+										imageType = 'image/png';
+									}
+
+									elt.src = 'data:' + imageType + ';base64,' + base64;
 								}
 								else {
 									throw new Error('xhrArrayBufferSrc directive only supports setting LINK href and IMG src');
 								}
+
+								window.lastTime = +(new Date());
+
+								//console.log('xhrArrayBufferSrc: ' + count++);
+								count++;
 							},
 							function(result) {
 								var data = typeof(result.data) === "object" ? JSON.stringify(result.data) : result.data;
